202741143d90f47689767bf9389e10576a192f8b
jhandles7 <77910911+jhandles7@users.noreply.github.com>
2021-05-10 19:48:33 +0800

    add codes to each number buttons, reset and decimal

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -46,7 +46,7 @@ namespace calculator
             this.delete_button = new System.Windows.Forms.Button();
             this.negative_Button = new System.Windows.Forms.Button();
             this.divide_operation = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OutputTxtBox = new System.Windows.Forms.TextBox();
             this.equal_button = new System.Windows.Forms.Button();
             this.add_operation = new System.Windows.Forms.Button();
             this.nine_button = new System.Windows.Forms.Button();
@@ -54,24 +54,38 @@ namespace calculator
             // 
             // zero_button
             // 
+            this.zero_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zero_button.AutoSize = true;
             this.zero_button.Location = new System.Drawing.Point(80, 313);
             this.zero_button.Name = "zero_button";
             this.zero_button.Size = new System.Drawing.Size(62, 44);
             this.zero_button.TabIndex = 1;
             this.zero_button.Text = "0";
             this.zero_button.UseVisualStyleBackColor = true;
+            this.zero_button.Click += new System.EventHandler(this.zero_button_Click);
             // 
             // decimal_button
             // 
+            this.decimal_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decimal_button.AutoSize = true;
             this.decimal_button.Location = new System.Drawing.Point(148, 313);
             this.decimal_button.Name = "decimal_button";
             this.decimal_button.Size = new System.Drawing.Size(62, 44);
             this.decimal_button.TabIndex = 2;
             this.decimal_button.Text = ".";
             this.decimal_button.UseVisualStyleBackColor = true;
+            this.decimal_button.Click += new System.EventHandler(this.decimal_button_Click);
             // 
             // Percent_button
             // 
+            this.Percent_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Percent_button.AutoSize = true;
             this.Percent_button.Location = new System.Drawing.Point(12, 313);
             this.Percent_button.Name = "Percent_button";
             this.Percent_button.Size = new System.Drawing.Size(62, 44);
@@ -81,78 +95,113 @@ namespace calculator
             // 
             // one_button
             // 
+            this.one_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.one_button.Location = new System.Drawing.Point(12, 263);
             this.one_button.Name = "one_button";
             this.one_button.Size = new System.Drawing.Size(62, 44);
             this.one_button.TabIndex = 6;
             this.one_button.Text = "1";
             this.one_button.UseVisualStyleBackColor = true;
+            this.one_button.Click += new System.EventHandler(this.one_button_Click);
             // 
             // four_button
             // 
+            this.four_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.four_button.Location = new System.Drawing.Point(12, 213);
             this.four_button.Name = "four_button";
             this.four_button.Size = new System.Drawing.Size(62, 44);
             this.four_button.TabIndex = 7;
             this.four_button.Text = "4";
             this.four_button.UseVisualStyleBackColor = true;
+            this.four_button.Click += new System.EventHandler(this.four_button_Click);
             // 
             // two_button
             // 
+            this.two_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.two_button.Location = new System.Drawing.Point(80, 263);
             this.two_button.Name = "two_button";
             this.two_button.Size = new System.Drawing.Size(62, 44);
             this.two_button.TabIndex = 8;
             this.two_button.Text = "2";
             this.two_button.UseVisualStyleBackColor = true;
+            this.two_button.Click += new System.EventHandler(this.two_button_Click);
             // 
             // five_button
             // 
+            this.five_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.five_button.Location = new System.Drawing.Point(80, 215);
             this.five_button.Name = "five_button";
             this.five_button.Size = new System.Drawing.Size(62, 42);
             this.five_button.TabIndex = 9;
             this.five_button.Text = "5";
             this.five_button.UseVisualStyleBackColor = true;
+            this.five_button.Click += new System.EventHandler(this.five_button_Click);
             // 
             // seven_button
             // 
+            this.seven_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.seven_button.Location = new System.Drawing.Point(12, 165);
             this.seven_button.Name = "seven_button";
             this.seven_button.Size = new System.Drawing.Size(62, 42);
             this.seven_button.TabIndex = 10;
             this.seven_button.Text = "7";
             this.seven_button.UseVisualStyleBackColor = true;
+            this.seven_button.Click += new System.EventHandler(this.seven_button_Click);
             // 
             // eight_button
             // 
+            this.eight_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.eight_button.Location = new System.Drawing.Point(80, 165);
             this.eight_button.Name = "eight_button";
             this.eight_button.Size = new System.Drawing.Size(62, 42);
             this.eight_button.TabIndex = 11;
             this.eight_button.Text = "8";
             this.eight_button.UseVisualStyleBackColor = true;
+            this.eight_button.Click += new System.EventHandler(this.eight_button_Click);
             // 
             // three_button
             // 
+            this.three_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.three_button.Location = new System.Drawing.Point(148, 263);
             this.three_button.Name = "three_button";
             this.three_button.Size = new System.Drawing.Size(62, 44);
             this.three_button.TabIndex = 12;
             this.three_button.Text = "3";
             this.three_button.UseVisualStyleBackColor = true;
+            this.three_button.Click += new System.EventHandler(this.three_button_Click);
             // 
             // six_button
             // 
+            this.six_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.six_button.Location = new System.Drawing.Point(148, 215);
             this.six_button.Name = "six_button";
             this.six_button.Size = new System.Drawing.Size(62, 42);
             this.six_button.TabIndex = 14;
             this.six_button.Text = "6";
             this.six_button.UseVisualStyleBackColor = true;
+            this.six_button.Click += new System.EventHandler(this.six_button_Click);
             // 
             // subtract_operation
             // 
+            this.subtract_operation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.subtract_operation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.subtract_operation.Location = new System.Drawing.Point(216, 215);
             this.subtract_operation.Name = "subtract_operation";
@@ -163,6 +212,9 @@ namespace calculator
             // 
             // reset_button
             // 
+            this.reset_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.reset_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.reset_button.Location = new System.Drawing.Point(12, 124);
             this.reset_button.Name = "reset_button";
@@ -170,9 +222,13 @@ namespace calculator
             this.reset_button.TabIndex = 16;
             this.reset_button.Text = "Reset";
             this.reset_button.UseVisualStyleBackColor = false;
+            this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
             // 
             // multiply_operation
             // 
+            this.multiply_operation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.multiply_operation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.multiply_operation.Location = new System.Drawing.Point(216, 165);
             this.multiply_operation.Name = "multiply_operation";
@@ -183,6 +239,9 @@ namespace calculator
             // 
             // delete_button
             // 
+            this.delete_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.delete_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.delete_button.Location = new System.Drawing.Point(80, 124);
             this.delete_button.Name = "delete_button";
@@ -193,6 +252,9 @@ namespace calculator
             // 
             // negative_Button
             // 
+            this.negative_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.negative_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.negative_Button.Location = new System.Drawing.Point(148, 124);
             this.negative_Button.Name = "negative_Button";
@@ -200,9 +262,13 @@ namespace calculator
             this.negative_Button.TabIndex = 20;
             this.negative_Button.Text = "+/-";
             this.negative_Button.UseVisualStyleBackColor = false;
+            this.negative_Button.Click += new System.EventHandler(this.negative_Button_Click);
             // 
             // divide_operation
             // 
+            this.divide_operation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.divide_operation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.divide_operation.Location = new System.Drawing.Point(216, 124);
             this.divide_operation.Name = "divide_operation";
@@ -211,21 +277,28 @@ namespace calculator
             this.divide_operation.Text = "/";
             this.divide_operation.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // OutputTxtBox
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(12, 29);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(284, 74);
-            this.textBox1.TabIndex = 22;
-            this.textBox1.Text = "0";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.OutputTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputTxtBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.OutputTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputTxtBox.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OutputTxtBox.Location = new System.Drawing.Point(12, 29);
+            this.OutputTxtBox.Multiline = true;
+            this.OutputTxtBox.Name = "OutputTxtBox";
+            this.OutputTxtBox.Size = new System.Drawing.Size(284, 74);
+            this.OutputTxtBox.TabIndex = 22;
+            this.OutputTxtBox.Text = "0";
+            this.OutputTxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // equal_button
             // 
+            this.equal_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.equal_button.AutoSize = true;
             this.equal_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.equal_button.Location = new System.Drawing.Point(216, 313);
             this.equal_button.Name = "equal_button";
@@ -236,6 +309,9 @@ namespace calculator
             // 
             // add_operation
             // 
+            this.add_operation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.add_operation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.add_operation.Location = new System.Drawing.Point(216, 263);
             this.add_operation.Name = "add_operation";
@@ -246,12 +322,16 @@ namespace calculator
             // 
             // nine_button
             // 
+            this.nine_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.nine_button.Location = new System.Drawing.Point(148, 165);
             this.nine_button.Name = "nine_button";
             this.nine_button.Size = new System.Drawing.Size(62, 42);
             this.nine_button.TabIndex = 17;
             this.nine_button.Text = "9";
             this.nine_button.UseVisualStyleBackColor = true;
+            this.nine_button.Click += new System.EventHandler(this.nine_button_Click);
             // 
             // Form1
             // 
@@ -261,7 +341,7 @@ namespace calculator
             this.ClientSize = new System.Drawing.Size(304, 378);
             this.Controls.Add(this.add_operation);
             this.Controls.Add(this.equal_button);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OutputTxtBox);
             this.Controls.Add(this.divide_operation);
             this.Controls.Add(this.negative_Button);
             this.Controls.Add(this.delete_button);
@@ -306,7 +386,7 @@ namespace calculator
         private System.Windows.Forms.Button delete_button;
         private System.Windows.Forms.Button negative_Button;
         private System.Windows.Forms.Button divide_operation;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox OutputTxtBox;
         private System.Windows.Forms.Button equal_button;
         private System.Windows.Forms.Button add_operation;
         private System.Windows.Forms.Button nine_button;
--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -12,6 +12,9 @@ namespace calculator
 {
     public partial class Form1 : Form
     {
+        
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -29,8 +32,145 @@ namespace calculator
 
         }
 
-        
 
-        
+        //Number buttons
+        private void zero_button_Click(object sender, EventArgs e)
+        {
+            OutputTxtBox.Text += "0";
+        }
+
+        private void one_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "1";
+            }
+            else
+            {
+                OutputTxtBox.Text += "1";
+            }
+            
+        }
+
+        private void two_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "2";
+            }
+            else
+            {
+                OutputTxtBox.Text += "2";
+            }
+        }
+
+        private void three_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "3";
+            }
+            else
+            {
+                OutputTxtBox.Text += "3";
+            }
+        }
+
+        private void four_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "4";
+            }
+            else
+            {
+                OutputTxtBox.Text += "4";
+            }
+        }
+
+        private void five_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "5";
+            }
+            else
+            {
+                OutputTxtBox.Text += "5";
+            }
+        }
+
+        private void six_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "6";
+            }
+            else
+            {
+                OutputTxtBox.Text += "6";
+            }
+        }
+
+        private void seven_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "7";
+            }
+            else
+            {
+                OutputTxtBox.Text += "7";
+            }
+        }
+
+        private void eight_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "8";
+            }
+            else
+            {
+                OutputTxtBox.Text += "8";
+            }
+        }
+
+        private void nine_button_Click(object sender, EventArgs e)
+        {
+            if (OutputTxtBox.Text == "0")
+            {
+                OutputTxtBox.Text = "9";
+            }
+            else
+            {
+                OutputTxtBox.Text += "9";
+            }
+        }
+
+
+        //Other button
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            OutputTxtBox.Text = "0";
+        }
+
+        private void decimal_button_Click(object sender, EventArgs e)
+        {
+                      
+            if (OutputTxtBox.Text.Contains("."))
+            {
+                
+            }
+            else
+            {
+                OutputTxtBox.Text += ".";
+            }
+
+        }
+
+        private void negative_Button_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
